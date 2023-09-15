ada30a73629deb68602c65fa945854265ce739e3
becca <becca@becca.com>
2023-09-15 17:19:29 +0800

    gan模型代码和senet注意力模块

---
+++ attention/Senet.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn
+
+
+# 注意力机制
+class Senet(nn.Module):
+    def __init__(self, channel, ratio=16):
+        super(Senet, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // ratio, False),
+            nn.ReLU(),
+            nn.Linear(channel // ratio, channel, False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+        avg = self.avg_pool(x).view([b, c])
+        print(avg.shape())
+        fc = self.fc(avg).view([b, c, 1, 1])
+        return x * fc
+
+
+model = Senet(512)
+inputs = torch.ones([2, 512, 26, 26])
+outputs = model(inputs)
